7ebc4603097fe91e6b517422dfc70b2275196c14
btien0211 <maibichtien7@gmail.com>
2026-02-03 09:56:01 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Mai Bích Tiên đã sẵn sàng!');
+    // Bạn có thể thêm các hiệu ứng JS khác tại đây nếu muốn
+});
